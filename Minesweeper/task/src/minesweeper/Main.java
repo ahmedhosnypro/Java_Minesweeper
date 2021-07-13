2c10dc25f184e16744f659c14fe0398874f3d400
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2021-07-13 20:05:50 +0200

    Initial commit

---
+++ Minesweeper/task/src/minesweeper/Main.java
@@ -0,0 +1,15 @@
+package minesweeper;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(".X.......");
+        System.out.println(".....X..X");
+        System.out.println("....X....");
+        System.out.println("......X..");
+        System.out.println("..X......");
+        System.out.println("....X....");
+        System.out.println("..X......");
+        System.out.println("..X......");
+        System.out.println("......X..");
+    }
+}
