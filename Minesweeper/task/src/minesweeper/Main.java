9626619e91ecb8ef46c4beebb56949879e48bef9
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2021-07-14 04:19:37 +0200

    Look around you

--- Minesweeper/task/src/minesweeper/Main.java
+++ Minesweeper/task/src/minesweeper/Main.java
@@ -1,14 +1,15 @@
 package minesweeper;
 
 import java.util.Scanner;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("How many mines do you want on the field? ");
         int mines = scanner.nextInt();
         MinesField minesField = new MinesField(mines);
-        System.out.println(minesField.getOptimizedMinesField());
+
+        System.out.println(minesField.getOptimizedHint());
     }
 }
 
--- Minesweeper/task/src/minesweeper/MinesField.java
+++ Minesweeper/task/src/minesweeper/MinesField.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class MinesField {
     Random random = new Random();
-    private char[][] minesField = new char[9][9];
+    private final char[][] minesField = new char[9][9];
 
     public MinesField(int mines) {
         for (char[] chars : minesField) {
@@ -23,13 +23,85 @@ public class MinesField {
         }
     }
 
-    public char[][] getMinesField() {
-        return minesField;
+    public String getOptimizedMinesField() {
+        StringBuilder out = new StringBuilder();
+        for (char[] chars : minesField) {
+            out.append(new String(chars));
+            out.append('\n');
+        }
+        return out.toString().trim();
     }
 
-    public String getOptimizedMinesField(){
+    private int[][] minesFieldToBinary() {
+        int[][] out = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (minesField[i][j] == 'X')
+                    out[i][j] = 1;
+                else
+                    out[i][j] = 0;
+            }
+        }
+        return out;
+    }
+
+    private char[][] getHint() {
+        int[][] binaryField = minesFieldToBinary();
+        char[][] hints = new char[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                int x;
+                if (binaryField[i][j] == 1) {
+                    hints[i][j] = 'X';
+                } else {
+                    if (i == 0 && j == 0) { //fst Corner
+                        x = binaryField[i][j + 1]
+                                + binaryField[i + 1][j] + binaryField[i + 1][j + 1];
+                    } else if (i == 0 && j == 8) { //snd Corner
+                        x = binaryField[i][j - 1] +
+                                binaryField[i + 1][j - 1] + binaryField[i + 1][j];
+                    } else if (i == 8 && j == 0) { //3rd Corner
+                        x = binaryField[i - 1][j] + binaryField[i - 1][j + 1]
+                                + binaryField[i][j + 1];
+                    } else if (i == 8 && j == 8) { // 4th Corner
+                        x = binaryField[i - 1][j - 1] + binaryField[i - 1][j]
+                                + binaryField[i][j - 1];
+                    } else if (i == 0) { //fst rom
+                        x = binaryField[i][j - 1] + binaryField[i][j + 1]
+                                + binaryField[i + 1][j - 1] + binaryField[i + 1][j] + binaryField[i + 1][j + 1];
+                    } else if (i == 8) { //last row
+                        x = binaryField[i - 1][j - 1] + binaryField[i - 1][j] + binaryField[i - 1][j + 1]
+                                + binaryField[i][j - 1] + binaryField[i][j + 1];
+
+                    } else if (j == 0) { // fst clm
+                        x = binaryField[i - 1][j] + binaryField[i - 1][j + 1]
+                                + binaryField[i][j + 1]
+                                + binaryField[i + 1][j] + binaryField[i + 1][j + 1];
+                    } else if (j == 8) { // last clm
+                        x = binaryField[i - 1][j - 1] + binaryField[i - 1][j]
+                                + binaryField[i][j - 1]
+                                + binaryField[i + 1][j - 1] + binaryField[i + 1][j];
+                    } else {
+                        x = binaryField[i - 1][j - 1] + binaryField[i - 1][j] + binaryField[i - 1][j + 1]
+                                + binaryField[i][j - 1] + binaryField[i][j + 1]
+                                + binaryField[i + 1][j - 1] + binaryField[i + 1][j] + binaryField[i + 1][j + 1];
+                    }
+                    if(x ==0)
+                        hints[i][j] = '.';
+                    else
+                        hints[i][j] = (char) (x +48);
+
+                }
+
+            }
+        }
+
+        return hints;
+    }
+
+    public String getOptimizedHint(){
         StringBuilder out = new StringBuilder();
-        for (char[] chars :minesField){
+        for (char[] chars : getHint()) {
             out.append(new String(chars));
             out.append('\n');
         }
